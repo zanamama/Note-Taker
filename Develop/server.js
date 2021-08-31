63a28216d489a0bd3418de570a4089ec45957612
zanama <57731386+zanmama@users.noreply.github.com>
2021-08-31 19:52:54 -0400

    syntax fix to api/notes route fetch

--- Develop/server.js
+++ Develop/server.js
@@ -19,7 +19,7 @@ app.get("notes", (req, res) => {
   res.sendFile(path.join(__dirname + "/public/notes.html"));
 });
 
-app.get("api/notes", (req, res) => {
+app.get("/api/notes", (req, res) => {
     res.sendFile(path.join(__dirname + "/public/api/notes.html"));
 });
 
