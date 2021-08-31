61dfb46c22fafbcc5df3cf78430645ea10273e6e
zanama <57731386+zanmama@users.noreply.github.com>
2021-08-31 19:36:37 -0400

    added homepage route and one app.use with extended:true

--- Develop/server.js
+++ Develop/server.js
@@ -2,11 +2,17 @@
 const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 3001;
-const path = require ("path");
-const {init, addNewNote, fetchNotes, removeNote} = require("./logic.js");
+const path = require("path");
+const { init, addNewNote, fetchNotes, removeNote } = require("./logic.js");
 
 app.use(express.static(__dirname + "/public/"));
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 //ROUTES
+// homepage route:
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname + "/public/index.html"));
+});
 
 //PUSH Changes to github after creating code. Because my heroku is connected to my github, I can click on deploy and it'll deploy to heroku
