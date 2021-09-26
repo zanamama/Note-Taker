9909fba201d419972298beb2b4ff0c7a1acc95aa
zanama <57731386+zanmama@users.noreply.github.com>
2021-09-26 12:10:35 -0400

    Removed unnecessary comments from server.js file

--- Develop/server.js
+++ Develop/server.js
@@ -1,4 +1,3 @@
-//DEPENDENCIES
 const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -9,8 +8,6 @@ app.use(express.static(__dirname + "/public/"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-//ROUTES
-// homepage route:
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname + "/public/index.html"));
 });
@@ -20,25 +17,24 @@ app.get("notes", (req, res) => {
 });
 
 app.post("/api/notes", (req, res) => {
-    const newNote = {id: Date.now(), title: req.body.title, text: req.body.text,};
-;
-    addNewNote(newNote);
-    res.send(newNote);
+  const newNote = {
+    id: Date.now(),
+    title: req.body.title,
+    text: req.body.text,
+  };
+  addNewNote(newNote);
+  res.send(newNote);
 });
 
 app.delete("/api/notes/:id", (req, res) => {
-    const noteId = req.params.id;
-    removeNote(nodeId);
-    res.send(`deleting ${noteId}`);
+  const noteId = req.params.id;
+  removeNote(nodeId);
+  res.send(`deleting ${noteId}`);
 });
 
-//Server listening port
 app.listen(PORT, (err) => {
-    if (err) throw err;
-    console.log(`Listening on PORT ${PORT}`);
+  if (err) throw err;
+  console.log(`Listening on PORT ${PORT}`);
 });
 
-//PUSH Changes to github after creating code. Because my heroku is connected to my github, I can click on deploy and it'll deploy to heroku
-
-init(); 
-//INITIALIZE aboveß
+init();
